568076a900d9afc654b972c9fe9101657db16e3b
Kenny Chen <kennygchen@yahoo.com>
2022-07-12 19:37:58 -0700

    install pygame as dependency

--- LuokeCollection/index.py
+++ LuokeCollection/index.py
@@ -1,1 +1,26 @@
+import pygame
+from pygame.locals import *
 from main import *
+
+pygame.init()
+screen = pygame.display.set_mode([500, 500])
+
+running = True
+while running:
+
+    # Did the user click the window close button?
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Fill the background with white
+    screen.fill((0, 0, 0))
+
+    # Draw a solid blue circle in the center
+    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+
+    # Flip the display
+    pygame.display.flip()
+
+# Done! Time to quit.
+pygame.quit()
