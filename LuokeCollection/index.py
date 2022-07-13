63381183aee6a9568b7aa8f64c9644a0b186cebd
Kenny Chen <kennygchen@yahoo.com>
2022-07-13 13:03:19 -0700

    Title change, icon change, mouse input recognize

--- LuokeCollection/index.py
+++ LuokeCollection/index.py
@@ -1,23 +1,35 @@
+from re import A
 import pygame
 from pygame.locals import *
 from main import *
 
 pygame.init()
-screen = pygame.display.set_mode([500, 500])
+GAME_RESOLUTION = (1080, 720)
+black = (0, 0, 0)
+screen = pygame.display.set_mode(GAME_RESOLUTION)
+Icon = pygame.image.load("assets\png\icon.png")
+pet = pygame.transform.scale(
+    pygame.image.load("assets/png/display.png"), (103.8, 148.9)
+)
+pygame.display.set_caption("Roco Collection 洛克王國 寵物圖鑑")
+pygame.display.set_icon(Icon)
+positions = []
 
 running = True
 while running:
-
     # Did the user click the window close button?
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONUP:
+            if event.button == 1:
+                positions.append(event.pos)
+            elif event.button == 3:
+                positions.clear()
+                screen.fill(black)
 
-    # Fill the background with white
-    screen.fill((0, 0, 0))
-
-    # Draw a solid blue circle in the center
-    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+    for position in positions:
+        screen.blit(pet, position)
 
     # Flip the display
     pygame.display.flip()
