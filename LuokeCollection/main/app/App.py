d1c164d080222254ce4ae0c709677adc94a98b28
Ruoqi Huang <huangruoqi374@gmail.com>
2022-07-12 19:42:28 -0700

    black

--- LuokeCollection/main/app/App.py
+++ LuokeCollection/main/app/App.py
@@ -1,10 +1,10 @@
 class App:
-    def __init__(self): 
+    def __init__(self):
         self.scene = None
         self.stack = []
 
     def change_scene(self, scene):
-        if self.stack: 
+        if self.stack:
             self.stack.pop()
         self.push_scene(scene)
         pass
@@ -12,11 +12,11 @@ class App:
     def push_scene(self, scene):
         self.scene = scene
         self.stack.append(scene)
-    
+
     def pop_scene(self):
         self.stack.pop()
-        if self.stack: 
-            self.scene = self.stack[len(self.stack)-1]
+        if self.stack:
+            self.scene = self.stack[len(self.stack) - 1]
 
     def display(self):
         self.scene.display()
