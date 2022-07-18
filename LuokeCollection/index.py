4cc2230150364e01fb6ccc00c616ed8797835db5
Kenny Chen <kennygchen@yahoo.com>
2022-07-17 23:31:59 -0700

    Custom cursor added

--- LuokeCollection/index.py
+++ LuokeCollection/index.py
@@ -1,4 +1,3 @@
-from re import A
 import pygame
 from pygame.locals import *
 from main import *
@@ -8,30 +7,41 @@ GAME_RESOLUTION = (1080, 720)
 black = (0, 0, 0)
 screen = pygame.display.set_mode(GAME_RESOLUTION)
 Icon = pygame.image.load("assets\png\icon.png")
+cursor_arrow = pygame.transform.scale(
+    pygame.image.load("assets/png/cursor.png"), (18.2, 27.2)
+)
+cursor_hand = pygame.transform.scale(
+    pygame.image.load("assets/png/hand.png"), (24, 26.6)
+)
 pet = pygame.transform.scale(
     pygame.image.load("assets/png/display.png"), (103.8, 148.9)
 )
+
 pygame.display.set_caption("Roco Collection 洛克王國 寵物圖鑑")
 pygame.display.set_icon(Icon)
 positions = []
 
+pygame.mouse.set_visible(False)
+
 running = True
+
 while running:
-    # Did the user click the window close button?
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.MOUSEBUTTONUP:
+        if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
                 positions.append(event.pos)
             elif event.button == 3:
                 positions.clear()
-                screen.fill(black)
+
+    screen.fill(black)
 
     for position in positions:
         screen.blit(pet, position)
 
-    # Flip the display
+    screen.blit(cursor_arrow, pygame.mouse.get_pos())
+
     pygame.display.flip()
 
 # Done! Time to quit.
