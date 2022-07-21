1bc2d3093924c8be0d25fc1707897d7527bd9c86
Ruoqi Huang <huangruoqi374@gmail.com>
2022-07-20 20:59:23 -0700

    refactor mouse display

--- LuokeCollection/index.py
+++ LuokeCollection/index.py
@@ -2,19 +2,13 @@ import pygame
 from pygame.locals import *
 from LuokeCollection.main.scenes.utils import vec
 from main.app import App
-from settings.dev import WIDTH, HEIGHT
+from settings.dev import WIDTH, HEIGHT, IMAGE
 
 
 pygame.init()
-black = (0, 0, 0)
 GAME_RESOLUTION = (WIDTH, HEIGHT)
 screen = pygame.display.set_mode(GAME_RESOLUTION)
-Icon = pygame.image.load("assets\png\icon.png")
-cursor_arrow = pygame.transform.scale(
-    pygame.image.load("assets/png/cursor.png"), (36, 54)
-)
-cursor_hand = pygame.transform.scale(pygame.image.load("assets/png/hand.png"), (48, 54))
-
+Icon = pygame.image.load(IMAGE('icon.png'))
 pygame.display.set_caption("Roco Collection 洛克王國 寵物圖鑑")
 pygame.display.set_icon(Icon)
 positions = []
@@ -24,36 +18,26 @@ app = App(screen)
 app.change_scene("init")
 clock = pygame.time.Clock()
 click_pos = vec(-1000, -1000)
+mouse_pos = vec(0, 0)
 pressing = False
 
 running = True
 while running:
-    # Did the user click the window close button?
-    click_pos.x = -1000
-    click_pos.y = -1000
+    click_pos.x, click_pos.y = -1000, -1000
+    mouse_pos.x, mouse_pos.y = pygame.mouse.get_pos()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONUP:
-            # if event.button == 1:
-            #     positions.append(event.pos)
-            # elif event.button == 3:
-            #     positions.clear()
-            #     screen.fill(black)
             if pressing:
-                click_pos.x = event.pos[0]
-                click_pos.y = event.pos[1]
+                click_pos.x, click_pos.y = event.pos
                 pressing = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             pressing = True
 
-    # for position in positions:
-    #     screen.blit(pet, position)
-
-    app.update(click_pos)
-    app.display()
+    app.update(mouse_pos, click_pos)
+    app.display(mouse_pos, click_pos)
     clock.tick(60)
-    screen.blit(cursor_arrow, pygame.mouse.get_pos())
     pygame.display.flip()
 
 # Done! Time to quit.
--- LuokeCollection/settings/dev.py
+++ LuokeCollection/settings/dev.py
@@ -1,2 +1,5 @@
+import os
 WIDTH = 1239
 HEIGHT = 826
+IMAGE = lambda name: os.path.join('LuokeCollection/assets/images/', name)
+SOUND = lambda name: os.path.join('LuokeCollection/assets/sounds/', name)
