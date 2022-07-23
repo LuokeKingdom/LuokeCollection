3c02efd07941f6d714bc052ca6c82af1b12c4521
Ruoqi Huang <huangruoqi374@gmail.com>
2022-07-22 20:35:23 -0700

    black

--- LuokeCollection/main/scenes/views/init_view.py
+++ LuokeCollection/main/scenes/views/init_view.py
@@ -8,12 +8,12 @@ from settings.dev import WIDTH, HEIGHT, IMAGE
 class InitView(View):
     BUTTONS = {
         "collection": Button(
-            image=IMAGE('collection_button.png'),
+            image=IMAGE("collection_button.png"),
             x=WIDTH / 5,
             y=HEIGHT / 2,
         )
     }
 
     def __init__(self, *args, **kwargs):
-        kwargs["bg"] = IMAGE('init_bg.jpg')
+        kwargs["bg"] = IMAGE("init_bg.jpg")
         super(InitView, self).__init__(*args, **kwargs)
--- LuokeCollection/settings/dev.py
+++ LuokeCollection/settings/dev.py
@@ -4,5 +4,7 @@ from pygame.locals import *
 
 WIDTH = 1239
 HEIGHT = 826
-IMAGE = lambda name: pygame.image.load(os.path.join("LuokeCollection/assets/images/", name))
+IMAGE = lambda name: pygame.image.load(
+    os.path.join("LuokeCollection/assets/images/", name)
+)
 SOUND = lambda name: os.path.join("LuokeCollection/assets/sounds/", name)
