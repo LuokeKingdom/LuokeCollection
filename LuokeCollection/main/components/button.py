c30a47c41089d5f19758e4a771af4a5f26e610c9
Ruoqi Huang <huangruoqi374@gmail.com>
2022-07-24 14:32:25 -0700

    resolve merge conflict

--- LuokeCollection/main/components/button.py
+++ LuokeCollection/main/components/button.py
@@ -1,11 +1,17 @@
 import pygame
 from pygame.locals import *
+
+from ..animation.button.button_animations import OpacityButtonAnimation
 from ..utils import vec
 from .container import Container
 
+import time
 
 class Button(Container):
-    def __init__(self, *args, **kwargs):
+    ANIMATIONS = {
+        'opacity': OpacityButtonAnimation,
+    }
+    def __init__(self, animation='opacity', *args, **kwargs):
         # Default button
         if len(args) < 1 and not kwargs.get("image"):
             image = pygame.Surface([100, 100])
@@ -13,9 +19,13 @@ class Button(Container):
             kwargs["image"] = image
         super().__init__(*args, **kwargs)
         self.on_click = None
+        self.hovered = False
+        self.animation = self.ANIMATIONS[animation](self)
 
     def is_click(self, click_pos):
         return self.rect.collidepoint(click_pos)
+    def check_collide(self, mouse_pos):
+        return self.rect.collidepoint(mouse_pos)
 
     def click(self):
         if self.on_click:
@@ -24,3 +34,13 @@ class Button(Container):
     def update(self):
         # can be used for animation probably
         pass
+    def update(self, mouse_pos, clicked):
+        current_time = time.time()
+        if self.check_collide(mouse_pos):
+            self.hovered = True
+            if clicked: self.click()
+        if self.hovered:
+            self.animation.play(current_time)
+        else:
+            self.animation.stop()
+        self.animation.update(current_time)
