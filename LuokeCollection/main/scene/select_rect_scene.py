4746b3ac5a13e29e113a802684ffcbd79267843c
Kenny Chen <kennygchen@yahoo.com>
2022-08-20 00:32:50 -0700

    change select area boundary to image rect

--- LuokeCollection/main/scene/select_rect_scene.py
+++ LuokeCollection/main/scene/select_rect_scene.py
@@ -1,3 +1,4 @@
+from pydoc import ModuleScanner
 import pygame
 from pygame.locals import *
 from .scene import Scene
@@ -32,10 +33,15 @@ class SelectRectScene(Scene):
         self.rate = 1
         self.shrink_rate = False
         self.rect = None
-        self.exit_point = 0
 
     def set_pet_image(self, image):
         self.OTHERS["image"].set_image(image, height=int(HEIGHT * 0.9))
+        (
+            self.image_rect_x,
+            self.image_rect_y,
+            self.image_rect_w,
+            self.image_rect_h,
+        ) = self.OTHERS["image"].image.get_rect()
 
     def side_effect(self):
         super().side_effect()
@@ -44,22 +50,32 @@ class SelectRectScene(Scene):
     def display(self, mouse_pos, clicked):
         super().display(mouse_pos, clicked)
         if mouse_pos.x < self.rect_side // 2:
-            if mouse_pos.y < self.rect_side // 2:
+            if mouse_pos.y < self.rect_side // 2:                           # when mouse_pos is at top left
                 self.rect_x = 0
                 self.rect_y = 0
-            elif mouse_pos.y > self.screen.get_size()[1] - self.rect_side // 2:
+            elif mouse_pos.y > self.image_rect_h - self.rect_side // 2:     # when mouse_pos is at bottom left
                 self.rect_x = 0
-                self.rect_y = self.screen.get_size()[1] - self.rect_side
-            else:
+                self.rect_y = self.image_rect_h - self.rect_side
+            else:                                                           # when mouse_pos is along left edge
                 self.rect_x = 0
                 self.rect_y = mouse_pos.y - self.rect_side // 2
-        elif mouse_pos.y < self.rect_side // 2:
+        elif mouse_pos.x > self.image_rect_w - self.rect_side // 2:
+            if mouse_pos.y < self.rect_side // 2:                           # when mouse_pos is at top right
+                self.rect_x = self.image_rect_w - self.rect_side
+                self.rect_y = 0
+            elif mouse_pos.y > self.image_rect_h - self.rect_side // 2:     # when mouse_pos is at bottom right
+                self.rect_x = self.image_rect_w - self.rect_side
+                self.rect_y = self.image_rect_h - self.rect_side
+            else:                                                           # when mouse_pos is along right edge
+                self.rect_x = self.image_rect_w - self.rect_side
+                self.rect_y = mouse_pos.y - self.rect_side // 2
+        elif mouse_pos.y < self.rect_side // 2:                             # when mouse_pos is along top edge
             self.rect_x = mouse_pos.x - self.rect_side // 2
             self.rect_y = 0
-        elif mouse_pos.y > self.screen.get_size()[1] - self.rect_side // 2:
+        elif mouse_pos.y > self.image_rect_h - self.rect_side // 2:         # when mouse_pos is along bottom edge
             self.rect_x = mouse_pos.x - self.rect_side // 2
-            self.rect_y = self.screen.get_size()[1] - self.rect_side
-        else:
+            self.rect_y = self.image_rect_h - self.rect_side
+        else:                                                               # when mouse_pos is not touching any edge
             self.rect_x = mouse_pos.x - self.rect_side // 2
             self.rect_y = mouse_pos.y - self.rect_side // 2
         pygame.draw.rect(
