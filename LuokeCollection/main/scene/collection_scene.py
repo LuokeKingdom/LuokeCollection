51d09159f10571ec818db51f36ab3e949433504b
Ruoqi Huang <huangruoqi374@gmail.com>
2022-08-22 19:10:37 -0700

    black

--- LuokeCollection/main/scene/collection_scene.py
+++ LuokeCollection/main/scene/collection_scene.py
@@ -92,9 +92,7 @@ class CollectionScene(Scene):
 
     def set_info(self, pet):
         self.TEXTS["pet_name"].change_text(pet.name)
-        pet_image = IMAGE(
-            os.path.join("assets/data/", pet.path, "display.png"), False
-        )
+        pet_image = IMAGE(os.path.join("assets/data/", pet.path, "display.png"), False)
         max_width, max_height = 380, 320
         w, h = pet_image.get_size()
         if h / max_height < w / max_width:
