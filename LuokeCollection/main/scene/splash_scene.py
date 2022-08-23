f1a5113b0fe27f8faeec4a3b3cafe9b814ea5c78
Kenny Chen <kennygchen@yahoo.com>
2022-08-23 10:21:15 -0700

    progress bar finish open init

--- LuokeCollection/main/scene/splash_scene.py
+++ LuokeCollection/main/scene/splash_scene.py
@@ -16,6 +16,7 @@ class SplashScene(Scene):
         kwargs["bg"] = pygame.Surface([1, 1])
         kwargs["bg"].fill((255, 255, 255))
         super(SplashScene, self).__init__(screen, model, *args, **kwargs)
+        self.progress = 0
         self.BUTTONS = {}
         self.OTHERS = {
             "icon": Container(
@@ -31,11 +32,24 @@ class SplashScene(Scene):
                 x=self.screen.get_width() // 2,
                 y=self.screen.get_height() // 2 + 60,
             ),
+            "progress_bar": Container(
+                image=IMAGE("progress_bar.png"),
+                align_mode="TOPLEFT",
+                x=0,
+                y=self.screen.get_height() - 50,
+            )
         }
         self.TEXTS = {}
 
     def display(self, mouse_pos, clicked):
         super().display(mouse_pos, clicked)
+        self.OTHERS["progress_bar"].set_image(self.OTHERS["progress_bar"].original_image, height = 100, width=self.progress)
+        self.OTHERS["progress_bar"].set_pos(x=0, y=self.screen.get_height() - 50)
+
 
     def update(self, mouse_pos, clicked):
         super().update(mouse_pos, clicked)
+        self.progress += 20
+        if self.progress > self.screen.get_width():
+            print("finish loading")
+            self.model.open("init")
