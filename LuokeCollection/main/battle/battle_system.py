90a5433dd8778a641bc6ee6f18fc26553a2324be
Ruoqi Huang <huangruoqi374@gmail.com>
2023-01-07 12:23:02 -0800

    add turn phases functions

--- LuokeCollection/main/battle/battle_system.py
+++ LuokeCollection/main/battle/battle_system.py
@@ -5,6 +5,23 @@ class BattleSystem:
         self.team1 = [None if args is None else BattlePet(*args) for args in pet_array_1]
         self.team2 = [None if args is None else BattlePet(*args) for args in pet_array_2]
 
-
     def get_pets(self):
         return self.team1[0], self.team2[0]
+
+    def prepare(self):
+        pass
+
+    def act(self):
+        if (self.preaction()):
+            self.action()
+        self.postaction()
+        pass
+
+    def preaction(self):
+        pass
+
+    def action(self):
+        pass
+
+    def postaction(self):
+        pass
